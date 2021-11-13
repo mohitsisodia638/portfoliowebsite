239ec01f0b3e2e661c95431ec3d63550d3c5e509
Mohit Sisodia <60215473+mohitsisodia638@users.noreply.github.com>
2021-11-13 09:50:25 +0530

    Equity Research Link added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
+//import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
+const Hero = () => (
   <>
     <Section row nopadding>
       <LeftSection>
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         About Me <br /> I am a highly competent final year student of NMIMS university with a proven track record. I have strong financial skills as well as excellent interpersonal skills. My greatest passion in life is using my knowledge and abilities for the benefit of the society.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
       </LeftSection>
     </Section>
   </>
